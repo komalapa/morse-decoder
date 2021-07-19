d39e1524ea051ad957691a7b9784fe79f7ca25ad
komalapa <komalapa@gmail.com>
2021-07-19 20:09:37 +0400

    refactor: rename variables by their purpose, const for letter length

--- src/index.js
+++ src/index.js
@@ -38,30 +38,29 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
+    const LETTER_LENGTH = 10
     // write your solution here
     const decoder = {'10' :'.', '11': '-', '00': ''};
     
     const words = expr.split('**********');
     
-    let morse = [];
+    let output = [];
     
     words.forEach(w => {
-        let morseWord = [];
-        for (let j = 0; j < w.length/10; j++){
-            let morseLetterBin = w.slice(10*j,10*(j+1));
+        let word = [];
+        for (let j = 0; j < w.length/LETTER_LENGTH; j++){
+            let morseLetterBin = w.slice(LETTER_LENGTH*j,LETTER_LENGTH*(j+1));
             let morseLetter = '';
             for (let i = 0; i<morseLetterBin.length/2; i++){
                 morseLetter += decoder[morseLetterBin.slice(2*i,2*(i+1))]
             }
-            //console.log(j, morseLetterBin)
-            morseWord.push(MORSE_TABLE[morseLetter])
+            word.push(MORSE_TABLE[morseLetter])
         }
         
-        morse.push(morseWord)
+        output.push(word)
     });
-    // console.log(words,morse)
-    morse = morse.map(w => w.join(''));
-    return morse.join(' ');
+    output = output.map(w => w.join(''));
+    return output.join(' ');
 }
 
 
