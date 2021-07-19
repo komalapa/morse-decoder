d9f6f4e78ecda6d91eb3f23d714831e7f9f32338
komalapa <komalapa@gmail.com>
2021-07-19 20:02:49 +0400

    feat: first solution

--- src/index.js
+++ src/index.js
@@ -39,7 +39,31 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    const decoder = {'10' :'.', '11': '-', '00': ''};
+    
+    const words = expr.split('**********');
+    
+    let morse = [];
+    
+    words.forEach(w => {
+        let morseWord = [];
+        for (let j = 0; j < w.length/10; j++){
+            let morseLetterBin = w.slice(10*j,10*(j+1));
+            let morseLetter = '';
+            for (let i = 0; i<morseLetterBin.length/2; i++){
+                morseLetter += decoder[morseLetterBin.slice(2*i,2*(i+1))]
+            }
+            //console.log(j, morseLetterBin)
+            morseWord.push(MORSE_TABLE[morseLetter])
+        }
+        
+        morse.push(morseWord)
+    });
+    // console.log(words,morse)
+    morse = morse.map(w => w.join(''));
+    return morse.join(' ');
 }
+
 
 module.exports = {
     decode
